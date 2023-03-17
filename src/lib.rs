c8ee8f840e206007b5a2d7aa588a3995d04ce34d
NutellaStory <mzmmmmmmmmmmmmm@gmail.com>
2023-03-17 18:27:32 +0900

    STEP_SIZE

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,62 @@ const BUTTON_RIGHT: u8 = 32;
 const BUTTON_UP: u8 = 64;
 const BUTTON_DOWN: u8 = 128;
 
+const STEP_SIZE: f32 = 0.045;
+
 extern "C" {
     fn vline(x: i32, y: i32, len: u32);
 }
+
+const MAP: [u16; 8] = [
+    0b1111111111111111,
+    0b1000001010000101,
+    0b1011100000110101,
+    0b1000111010010001,
+    0b1010001011110111,
+    0b1011101001100001,
+    0b1000100000001101,
+    0b1111111111111111,
+];
+
+struct State {
+    player_x: f32,
+    player_y: f32,
+    player_angle: f32,
+}    
+
+impl State {
+    pub fn update(&mut self, up: bool, down: bool, left: bool, right: bool) {
+        let previous_position = (self.player_x, self.player_y);
+
+        if up {
+            self.player_x += cosf(self.player_angle) * STEP_SIZE;
+            self.player_y += -sinf(self.player_angle) * STEP_SIZE;
+        }
+
+        if down {
+            self.player_x -= cosf(self.player_angle) * STEP_SIZE;
+            self.player_y -= -sinf(self.player_angle) * STEP_SIZE;
+        }
+
+        if right {
+            self.player_angle -= STEP_SIZE;
+        }
+
+        if left {
+            self.player_angle += STEP_SIZE;
+        }
+
+        if point_in_wall(self.player_x, self.player_y) {
+            (self.player_x, self.player_y) = previous_position;
+        }
+    }
+}
+
+static mut STATE: State = State {
+    player_x: 1.5,
+    player_y: 1.5,
+    player_angle: 0.0,
+};
 
 #[panic_handler]
 fn phandler(_: &PanicInfo<'_>) -> ! {
@@ -24,6 +77,15 @@ unsafe fn update() {
         vline(80, 20, 120);
     }
 }
+
+/// Check if the map contains a wall at a point.
+fn point_in_wall(x: f32, y: f32) -> bool {
+    match MAP.get(y as usize) {
+        Some(line) => (line & (0b1 << x as usize)) != 0,
+        None => true,
+    }
+}
+
 /*
 pub fn add(left: usize, right: usize) -> usize {
     left + right
